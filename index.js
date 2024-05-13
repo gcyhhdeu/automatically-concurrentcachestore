c24dc6e4aa9cae847c322733adf899a9e38c5a79
gcyhhdeu <js1iamrelly@hotmail.com>
2024-05-14 03:54:38 +0700

    test commit

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-const bubbleSort = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-      }
-    }
+const fibonacci = (n) => {
+  const sequence = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    sequence.push(sequence[i - 1] + sequence[i - 2]);
   }
-  return arr;
+  return sequence;
 };
