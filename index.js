34f0a559cec3fa7bb994b22015e927cf3f9e8521
gcyhhdeu <js1iamrelly@hotmail.com>
2024-05-14 14:21:59 +0700

    aptos task

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-const fibonacci = (n) => {
-  const sequence = [0, 1];
-  for (let i = 2; i <= n; i++) {
-    sequence.push(sequence[i - 1] + sequence[i - 2]);
-  }
-  return sequence;
-};
+// Setup the client
+const aptosConfig = new AptosConfig({ network: APTOS_NETWORK });
+const aptos = new Aptos(aptosConfig);
