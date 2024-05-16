85000afc561c04e33de46bd8ece759ed55c99648
gcyhhdeu <js1iamrelly@hotmail.com>
2024-05-16 08:45:30 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const factorialRecursive = (n) => {
-  if (n === 0 || n === 1) {
-    return 1;
+function sortColors(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+  let i = 0;
+  while (i <= right) {
+    if (nums[i] === 0) {
+      [nums[i], nums[left]] = [nums[left], nums[i]];
+      left++;
+      i++;
+    } else if (nums[i] === 2) {
+      [nums[i], nums[right]] = [nums[right], nums[i]];
+      right--;
+    } else {
+      i++;
+    }
   }
-  return n * factorialRecursive(n - 1);
-};
+}
